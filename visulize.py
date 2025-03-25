189fda10063b1a04f63f98f2dd85cd7616550c64
MonojitBairagi <monojitbairagi0@gmail.com>
2025-03-25 23:41:08 +0530

    visulize prediction

---
+++ visulize.py
@@ -0,0 +1,11 @@
+def visualize_results(actual_stock_price, predicted_stock_price):
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(10,5))
+    plt.plot(actual_stock_price, color='red', label='Actual Stock Price')
+    plt.plot(predicted_stock_price, color='blue', label='Predicted Stock Price')
+    plt.title('Stock Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.legend()
+    plt.show()
